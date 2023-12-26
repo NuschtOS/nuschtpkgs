818d2b2a6546163d3cac5f2f5eba9d4b50d3f513
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-12-26 04:01:43 +0100

    nixos/nginx: always link config file

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -290,11 +290,7 @@ let
     ${cfg.appendConfig}
   '';
 
-  configPath = if cfg.enableReload
-    then "/etc/nginx/nginx.conf"
-    else configFile;
-
-  execCommand = "${cfg.package}/bin/nginx -c '${configPath}'";
+  execCommand = "${cfg.package}/bin/nginx -c /etc/nginx/nginx.conf";
 
   vhosts = concatStringsSep "\n" (mapAttrsToList (vhostName: vhost:
     let
@@ -1300,9 +1296,7 @@ in
       };
     };
 
-    environment.etc."nginx/nginx.conf" = mkIf cfg.enableReload {
-      source = configFile;
-    };
+    environment.etc."nginx/nginx.conf".source = configFile;
 
     # This service waits for all certificates to be available
     # before reloading nginx configuration.
