e226c5e56e0db35b94e463af5d01ab495c5e8ebf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-12-26 04:01:43 +0100

    nixos/nginx: always link config file
    (cherry picked from commit 9a9168f6e4ca33f925e94c0728c5b08a755acbd2)

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -324,9 +324,7 @@ let
         ${cfg.appendConfig}
       '';
 
-  configPath = if cfg.enableReload then "/etc/nginx/nginx.conf" else configFile;
-
-  execCommand = "${cfg.package}/bin/nginx -c '${configPath}'";
+  execCommand = "${cfg.package}/bin/nginx -c /etc/nginx/nginx.conf";
 
   vhosts = concatStringsSep "\n" (
     mapAttrsToList (
@@ -1500,7 +1498,7 @@ in
           ${cfg.preStart}
           ${execCommand} -t
         '';
-
+        restartTriggers = optionals (!cfg.enableReload) [ configFile ];
         startLimitIntervalSec = 60;
         serviceConfig = {
           ExecStart = execCommand;
@@ -1641,9 +1639,7 @@ in
       )
     );
 
-    environment.etc."nginx/nginx.conf" = mkIf cfg.enableReload {
-      source = configFile;
-    };
+    environment.etc."nginx/nginx.conf".source = configFile;
 
     security.acme.certs =
       let
