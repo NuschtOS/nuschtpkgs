851ea463387378da17c5e74a7ed510287611a3d5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-03-14 19:32:54 +0100

    nixos/mediawiki: symlink config to /etc/

--- nixos/modules/services/web-apps/mediawiki.nix
+++ nixos/modules/services/web-apps/mediawiki.nix
@@ -762,6 +762,9 @@ in
     };
     users.groups.${group} = { };
 
-    environment.systemPackages = [ mediawikiScripts ];
+    environment = {
+      etc."mediawiki/LocalSettings.php".source = mediawikiConfig;
+      systemPackages = [ mediawikiScripts ];
+    };
   };
 }
