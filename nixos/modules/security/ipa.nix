5cd9e59d890c8f4d7dd0e23427e5f4b7f7de2ab1
Benjamin Staffin <benley@gmail.com>
2024-05-06 14:04:06 -0400

    nixos/ipa: Enable sudo and authorized_keys integration
    (cherry picked from commit 3f1dd11890200f39efd0481960179f47f56babfa)
    (cherry picked from commit 386c9d493b6553dec04ea4cd72448e6142fa0577)

--- nixos/modules/security/ipa.nix
+++ nixos/modules/security/ipa.nix
@@ -260,6 +260,8 @@ in
 
     services.sssd = {
       enable = true;
+      sshAuthorizedKeysIntegration = true;
+      sudoIntegration = true;
       settings = {
         "domain/${cfg.domain}" = {
           id_provider = "ipa";
