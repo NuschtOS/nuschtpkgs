7ae51a92e7ce179952da0727499095413c0d5514
Benjamin Staffin <benley@gmail.com>
2024-05-06 14:03:08 -0400

    sudo: Add sudoWithSssd variant
    (cherry picked from commit 9d95171f287349d7ef4bdf880a4397deba88838c)
    (cherry picked from commit 0a2a48899be5863bbb9a22e508bdbac5424521aa)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9893,6 +9893,10 @@ with pkgs;
     pythonBindings = true;
   };
 
+  sudoWithSssd = sudo.override {
+    withSssd = true;
+  };
+
   supersonic-wayland = supersonic.override {
     waylandSupport = true;
   };
