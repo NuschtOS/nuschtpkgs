8e35652e1742a2274673277e802c6034d2c87996
Benjamin Staffin <benley@gmail.com>
2024-05-10 15:04:43 -0400

    nixos/sssd: Enable nss netgroup lookups
    (cherry picked from commit 194aa2f97e1a67bbd8c31422bfad86af108573aa)

--- nixos/modules/services/misc/sssd.nix
+++ nixos/modules/services/misc/sssd.nix
@@ -214,6 +214,7 @@ in
         passwd = [ "sss" ];
         services = [ "sss" ];
         shadow = [ "sss" ];
+        netgroup = [ "sss" ];
       };
       services.dbus.packages = [ pkgs.sssd ];
     })
