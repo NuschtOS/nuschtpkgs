8c0530bb1027c771da8f0691c44540a17b3479bd
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-23 20:33:31 +0200

    mobilizon: patch ldap login

--- pkgs/servers/mobilizon/common.nix
+++ pkgs/servers/mobilizon/common.nix
@@ -1,14 +1,20 @@
-{ fetchFromGitLab }:
+{ applyPatches, fetchFromGitLab }:
 rec {
 
   pname = "mobilizon";
   version = "5.1.2";
 
-  src = fetchFromGitLab {
-    domain = "framagit.org";
-    owner = "framasoft";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-5xHLk5/ogtRN3mfJPP1/gIVlALerT9KEUHjLA2Ou3aM=";
+  src = applyPatches {
+    src = fetchFromGitLab {
+      domain = "framagit.org";
+      owner = "framasoft";
+      repo = pname;
+      rev = version;
+      sha256 = "sha256-5xHLk5/ogtRN3mfJPP1/gIVlALerT9KEUHjLA2Ou3aM=";
+    };
+
+    patches = [
+      ./allow-ldap-login-with-username.diff
+    ];
   };
 }
