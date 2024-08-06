523171b2afe4de4e7b18d1657b0bc75df3c297c1
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-08-06 19:06:54 +0200

    pretalx: 2024.1.0 -> 2024.2.0
    https://docs.pretalx.org/changelog/#v2024-2-0

--- pkgs/by-name/pr/pretalx/package.nix
+++ pkgs/by-name/pr/pretalx/package.nix
@@ -4,7 +4,6 @@
   gettext,
   python3,
   fetchFromGitHub,
-  fetchpatch2,
   plugins ? [ ],
   nixosTests,
 }:
@@ -30,13 +29,13 @@ let
     };
   };
 
-  version = "2024.1.0";
+  version = "2024.2.0";
 
   src = fetchFromGitHub {
     owner = "pretalx";
     repo = "pretalx";
     rev = "v${version}";
-    hash = "sha256-rFOlovybaEZnv5wBx6Dv8bVkP1D+CgYAKRXuNb6hLKQ=";
+    hash = "sha256-2HkxFS+T/lN/8EvAL3S4iVYn30y0OPmaUSneEPEA62k=";
   };
 
   meta = with lib; {
@@ -55,7 +54,7 @@ let
 
     sourceRoot = "${src.name}/src/pretalx/frontend/schedule-editor";
 
-    npmDepsHash = "sha256-B9R3Nn4tURNxzeyLDHscqHxYOQK9AcmDnyNq3k5WQQs=";
+    npmDepsHash = "sha256-EAdeXdcC3gHun6BOHzvqpzv9+oDl1b/VTeNkYLiD+hA=";
 
     npmBuildScript = "build";
 
@@ -70,18 +69,6 @@ python.pkgs.buildPythonApplication rec {
   outputs = [
     "out"
     "static"
-  ];
-
-  patches = [
-    # Fix persisent XSS in Markdown Preview
-    # https://github.com/pretalx/pretalx/commit/58e9990d4e2c81d1eb9a23f77801efcf5155ac08
-    ./0001-Sanitize-markdown-preview.patch
-    (fetchpatch2 {
-      # Backport support for Djangorestframework 3.15.x
-      name = "pretalx-drf-3.15.patch";
-      url = "https://github.com/pretalx/pretalx/commit/43a0416c6968d64ea57720abdb82f482940b11f8.patch";
-      hash = "sha256-Iw1xVF7j7c712kwIk1SMbQSF0ixMUZr1BJib3KAb2HY=";
-    })
   ];
 
   postPatch = ''
@@ -126,6 +113,7 @@ python.pkgs.buildPythonApplication rec {
       csscompressor
       cssutils
       defusedcsv
+      defusedxml
       django
       django-bootstrap4
       django-compressor
