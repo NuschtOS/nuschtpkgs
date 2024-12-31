piotrkwiecinski/nixpkgs

build-support/php: implement v2

pkgs/build-support/php/builders/default.nix
@@ -5,4 +5,10 @@
     mkComposerRepository = callPackage ./v1/build-composer-repository.nix { };
     composerHooks = callPackages ./v1/hooks { };
   };
+
+  v2 = {
+    buildComposerProject = callPackage ./v2/build-composer-project.nix { };
+    mkComposerRepository = callPackage ./v2/build-composer-repository.nix { };
+    composerHooks = callPackages ./v2/hooks { };
+  };
 }

pkgs/top-level/php-packages.nix
@@ -59,6 +59,11 @@ in {
 
   inherit (builders.v1) buildComposerProject composerHooks mkComposerRepository;
 
+  # Next version of the builder
+  buildComposerProject2 = builders.v2.buildComposerProject;
+  composerHooks2 = builders.v2.composerHooks;
+  mkComposerRepository2 = builders.v2.mkComposerRepository;
+
   # Wrap mkDerivation to prepend pname with "php-" to make names consistent
   # with how buildPecl does it and make the file easier to overview.
   mkDerivation = origArgs:

NuschtOS/nuschtpkgs

pkgs/build-support/php/builders/default.nix
@@ -6,4 +6,10 @@
     mkComposerRepository = callPackage ./v1/build-composer-repository.nix { };
     composerHooks = callPackages ./v1/hooks { };
   };
+
+  v2 = {
+    buildComposerProject = callPackage ./v2/build-composer-project.nix { };
+    mkComposerVendor = callPackage ./v2/build-composer-vendor.nix { };
+    composerHooks = callPackages ./v2/hooks { };
+  };
 }

pkgs/top-level/php-packages.nix
@@ -73,6 +73,11 @@ lib.makeScope pkgs.newScope (
       mkComposerRepository
       ;
 
+    # Next version of the builder
+    buildComposerProject2 = builders.v2.buildComposerProject;
+    composerHooks2 = builders.v2.composerHooks;
+    mkComposerVendor = builders.v2.mkComposerVendor;
+
     # Wrap mkDerivation to prepend pname with "php-" to make names consistent
     # with how buildPecl does it and make the file easier to overview.
     mkDerivation =