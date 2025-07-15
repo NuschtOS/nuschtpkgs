2ecc137323e536e18c5152d180d61020875a823e
Patrick Steinhardt <ps@pks.im>
2025-07-15 09:07:40 +0200

    msmtp: migrate to by-name
    (cherry picked from commit 30ad5fbf9da82a598dd69529b4fbdc599c0bc47c)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13557,10 +13557,6 @@ with pkgs;
 
   taxi-cli = with python3Packages; toPythonApplication taxi;
 
-  msmtp = callPackage ../applications/networking/msmtp {
-    autoreconfHook = buildPackages.autoreconfHook269;
-  };
-
   imapfilter = callPackage ../applications/networking/mailreaders/imapfilter.nix {
     lua = lua5;
   };
