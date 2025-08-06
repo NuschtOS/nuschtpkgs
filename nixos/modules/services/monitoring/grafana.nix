4107c88d4a0baca3c0c3a7938804d480ba22cdf9
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-06 16:22:24 +0200

    nixos/grafana: link config somewhere reasonable
    (cherry picked from commit a080c1fc96673c17d2914b6155c6526c8c507b90)

--- nixos/modules/services/monitoring/grafana.nix
+++ nixos/modules/services/monitoring/grafana.nix
@@ -2091,7 +2091,7 @@ in
         set -o errexit -o pipefail -o nounset -o errtrace
         shopt -s inherit_errexit
 
-        exec ${lib.getExe cfg.package} server -homepath ${cfg.dataDir} -config ${configFile}
+        exec ${lib.getExe cfg.package} server -homepath ${cfg.dataDir} -config  ${cfg.dataDir}/config.ini
       '';
       serviceConfig = {
         WorkingDirectory = cfg.dataDir;
@@ -2139,6 +2139,7 @@ in
       preStart = ''
         ln -fs ${cfg.package}/share/grafana/conf ${cfg.dataDir}
         ln -fs ${cfg.package}/share/grafana/tools ${cfg.dataDir}
+        ln -fs ${configFile} ${cfg.dataDir}/config.ini
       '';
     };
 
