3384a5117f6658c1fbe87bae55050ab6f6a71031
Marcel <me@m4rc3l.de>
2025-09-07 21:13:37 +0200

    flaresolverr: 3.3.25 -> 3.4.0

--- pkgs/by-name/fl/flaresolverr/package.nix
+++ pkgs/by-name/fl/flaresolverr/package.nix
@@ -17,38 +17,32 @@ let
   python = python3.withPackages (
     ps: with ps; [
       bottle
+      waitress
+      selenium
       func-timeout
       prometheus-client
-      selenium
-      waitress
-      xvfbwrapper
-
-      # For `undetected_chromedriver`
-      looseversion
       requests
+      certifi
       websockets
+      packaging
+      xvfbwrapper
     ]
   );
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "flaresolverr";
-  version = "3.3.25";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "FlareSolverr";
     repo = "FlareSolverr";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-AGRqJOIIePaJH0j0eyMFJ6Kddul3yXF6uw6dPMnskmY=";
+    hash = "sha256-iPhPwtidIeWzKeqs/efCFa8vs4uJaNNHlpdw9mVtwHw=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   postPatch = ''
-    substituteInPlace src/undetected_chromedriver/patcher.py \
-      --replace-fail \
-        "from distutils.version import LooseVersion" \
-        "from looseversion import LooseVersion"
-
     substituteInPlace src/utils.py \
       --replace-fail \
         'CHROME_EXE_PATH = None' \
