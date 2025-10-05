7c160b486211b76d75a8bf2ed43073578fd482b9
Marcel <me@m4rc3l.de>
2025-10-05 23:12:38 +0200

    nixos/sssd: migrate to rfc42-style settings
    (cherry picked from commit 41fe9d47d4a68711173502b34a6d73306fbcc09f)

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1255,8 +1255,8 @@ in
   ssh-agent-auth = handleTest ./ssh-agent-auth.nix { };
   ssh-audit = handleTest ./ssh-audit.nix { };
   sslh = handleTest ./sslh.nix { };
-  sssd = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./sssd.nix { };
   sssd-ldap = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./sssd-ldap.nix { };
+  sssd-legacy-config = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./sssd-legacy-config.nix { };
   stalwart-mail = handleTest ./stalwart-mail.nix { };
   stargazer = runTest ./web-servers/stargazer.nix;
   starship = runTest ./starship.nix;
