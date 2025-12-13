b9f19d1b3e5c54bdb9c24c4999b9b2a6d1467f45
Marcel <me@m4rc3l.de>
2025-12-13 02:02:00 +0100

    nixos/rustfs: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1505,6 +1505,7 @@ in
   rtkit = runTest ./rtkit.nix;
   rtorrent = runTest ./rtorrent.nix;
   rush = runTest ./rush.nix;
+  rustfs = runTest ./rustfs.nix;
   rustical = runTest ./web-apps/rustical.nix;
   rustls-libssl = runTest ./rustls-libssl.nix;
   rxe = runTest ./rxe.nix;
