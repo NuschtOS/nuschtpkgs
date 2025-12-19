ba62fbd94e9c6aeebe564e1bb0bbf2adf8dcd16b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-19 04:27:39 +0100

    paperless-ngx: 2.20.2 -> 2.20.3
    Diff: https://github.com/paperless-ngx/paperless-ngx/compare/v2.20.2...v2.20.3
    
    Changelog: https://github.com/paperless-ngx/paperless-ngx/releases/tag/v2.20.3
    (cherry picked from commit dc7bc0a345a460d4553c5a6a46ec0ea6c4ca14b4)

--- pkgs/by-name/pa/paperless-ngx/package.nix
+++ pkgs/by-name/pa/paperless-ngx/package.nix
@@ -31,13 +31,13 @@
   xorg,
 }:
 let
-  version = "2.20.2";
+  version = "2.20.3";
 
   src = fetchFromGitHub {
     owner = "paperless-ngx";
     repo = "paperless-ngx";
     tag = "v${version}";
-    hash = "sha256-jsHGQ1xjHu+yagy73/eCB/3h7ZHasMdt4rWQuFvDfis=";
+    hash = "sha256-aAcE0AUkB5SS4jwFOKCM7+iqc7EqGJv0qjqz0mnj2Wo=";
   };
 
   python = python3.override {
@@ -159,11 +159,6 @@ python.pkgs.buildPythonApplication rec {
   inherit version src;
 
   patches = [
-    (fetchpatch {
-      name = "GHSA-7cq3-mhxq-w946.patch";
-      url = "https://github.com/paperless-ngx/paperless-ngx/commit/bf38ae98f1ac3bae2c6006888a8705e42fbb804f.patch";
-      hash = "sha256-ATjtB7dmrXk/R+zjc0y2jJkmvVN7Gmqf0aWMRG9EN7I=";
-    })
     (fetchpatch {
       name = "GHSA-28cf-xvcf-hw6m.patch";
       url = "https://github.com/paperless-ngx/paperless-ngx/commit/7c457466b76d7a4abeca521043de69d3c1f4eb11.patch";
