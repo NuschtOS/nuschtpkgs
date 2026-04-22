e34cf56b485f55f029f843a1b6d07bd367e71e50
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-23 01:58:14 +0200

    python314Packages.samplerate-ledfx: init at 0.2.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16667,6 +16667,10 @@ self: super: with self; {
 
   samplerate = callPackage ../development/python-modules/samplerate { inherit (pkgs) libsamplerate; };
 
+  samplerate-ledfx = callPackage ../development/python-modules/samplerate-ledfx {
+    inherit (pkgs) libsamplerate;
+  };
+
   samsungctl = callPackage ../development/python-modules/samsungctl { };
 
   samsungtvws = callPackage ../development/python-modules/samsungtvws { };
