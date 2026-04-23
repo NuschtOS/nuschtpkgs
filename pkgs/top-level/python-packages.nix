ddc4ef3d48874e2240ea8c39e20eb5fd65eb52fd
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-23 03:02:05 +0200

    python314Packages.lifx-async: init at 5.4.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8610,6 +8610,8 @@ self: super: with self; {
 
   lifelines = callPackage ../development/python-modules/lifelines { };
 
+  lifx-async = callPackage ../development/python-modules/lifx-async { };
+
   lifx-emulator-core = callPackage ../development/python-modules/lifx-emulator-core { };
 
   lightgbm = callPackage ../development/python-modules/lightgbm { };
