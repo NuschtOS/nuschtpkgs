6856007222299b18a8843ef11d22e09f5531f798
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-06-22 13:31:53 +0200

    python314Packages.types-shapely: init at 2.1.0.20260603

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20685,6 +20685,8 @@ self: super: with self; {
 
   types-setuptools = callPackage ../development/python-modules/types-setuptools { };
 
+  types-shapely = callPackage ../development/python-modules/types-shapely { };
+
   types-six = callPackage ../development/python-modules/types-six { };
 
   types-tabulate = callPackage ../development/python-modules/types-tabulate { };
