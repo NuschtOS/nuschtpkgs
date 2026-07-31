2fea4da77fe5df69409445be930eddcc13b75f5a
Marcel <me@m4rc3l.de>
2026-07-31 10:36:44 +0200

    sdocx2pdf: 0.2.3 -> 0.3.0
    Diff: https://github.com/squ1dd13/sdocx2pdf/compare/v0.2.3...v0.3.0
    
    Changelog: https://github.com/squ1dd13/sdocx2pdf/releases/tag/v0.3.0

--- pkgs/by-name/sd/sdocx2pdf/package.nix
+++ pkgs/by-name/sd/sdocx2pdf/package.nix
@@ -7,17 +7,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sdocx2pdf";
-  version = "0.2.3";
+  version = "0.3.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "squ1dd13";
     repo = "sdocx2pdf";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-O22Kdi9kwpTcKGlg4gl/AMfp8au3+4PTIbN0IE7tX0w=";
+    hash = "sha256-sDYl2dhnayoyLbqx/SzMXvZ7m1bFZo8ufoQ7Lwpqf0Y=";
   };
 
-  cargoHash = "sha256-hnvfiY7DoPC22sqJxfj5Z9lUa8t/BOMuo9MEjKXSqhs=";
+  cargoHash = "sha256-rwhC6AITvrhIyqk2b1ew1Pc/0pn4c+LlFuMcCpwqipc=";
 
   passthru.updateScript = nix-update-script { };
 
