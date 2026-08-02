3dc59f6dac4175434544479c62d84c71dff628b9
Marcel <me@m4rc3l.de>
2026-08-02 17:43:58 +0200

    rustfs-rc: 0.1.20 -> 0.1.30
    Diff: https://github.com/rustfs/cli/compare/v0.1.20...v0.1.30
    
    Changelog: https://github.com/rustfs/cli/releases/tag/v0.1.30

--- pkgs/by-name/ru/rustfs-rc/package.nix
+++ pkgs/by-name/ru/rustfs-rc/package.nix
@@ -7,17 +7,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rustfs-rc";
-  version = "0.1.20";
+  version = "0.1.30";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "rustfs";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-arANPm11/CYc9FrT1/jw5Virl+cOiKpwI2gh/zelSnk=";
+    hash = "sha256-/z97K11fGRNv8XqiT6jOokqTu6RMpbq31xHUexefboM=";
   };
 
-  cargoHash = "sha256-fjx+W0y5nhbXYntmIlRBfxDqdHTYFmwyG+AEx1o9/j4=";
+  cargoHash = "sha256-5Jj/FjDd0Dn5IYdYDil0kVGUp8WBVWaCV7+mw4vO/e4=";
 
   passthru.updateScript = nix-update-script { };
 
