SuperSandro2000/nixpkgs

nixos/knot: always use config from /etc/knot/knot.conf

nixos/modules/services/networking/knot.nix
@@ -210,14 +210,14 @@ let
     buildCommand = ''
       mkdir -p $out/bin
       makeWrapper ${cfg.package}/bin/knotc "$out/bin/knotc" \
-        --add-flags "--config=${configFile}" \
+        --add-flags "--config=/etc/knot/knot.con" \
         --add-flags "--socket=${socketFile}"
       makeWrapper ${cfg.package}/bin/keymgr "$out/bin/keymgr" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.con"
       makeWrapper ${cfg.package}/bin/kzonesign "$out/bin/kzonesign" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.con"
       makeWrapper ${cfg.package}/bin/kcatalogprint "$out/bin/kcatalogprint" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.con"
       for executable in kdig khost kjournalprint knsec3hash knsupdate kzonecheck kxdpgun
       do
         ln -s "${cfg.package}/bin/$executable" "$out/bin/$executable"
@@ -350,7 +350,7 @@ in
           ExecStart = escapeSystemdExecArgs (
             [
               (lib.getExe cfg.package)
-              "--config=${configFile}"
+              "--config=/etc/knot/knot.conf"
               "--socket=${socketFile}"
             ]
             ++ cfg.extraArgs

NuschtOS/nuschtpkgs

(cherry picked from commit b7558120b645ca5e9b9bd32ae6b5a115a0bca19e)

nixos/modules/services/networking/knot.nix
@@ -216,14 +216,14 @@ let
     buildCommand = ''
       mkdir -p $out/bin
       makeWrapper ${cfg.package}/bin/knotc "$out/bin/knotc" \
-        --add-flags "--config=${configFile}" \
+        --add-flags "--config=/etc/knot/knot.conf" \
         --add-flags "--socket=${socketFile}"
       makeWrapper ${cfg.package}/bin/keymgr "$out/bin/keymgr" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.conf"
       makeWrapper ${cfg.package}/bin/kzonesign "$out/bin/kzonesign" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.conf"
       makeWrapper ${cfg.package}/bin/kcatalogprint "$out/bin/kcatalogprint" \
-        --add-flags "--config=${configFile}"
+        --add-flags "--config=/etc/knot/knot.conf"
       for executable in kdig khost kjournalprint knsec3hash knsupdate kzonecheck kxdpgun
       do
         ln -s "${cfg.package}/bin/$executable" "$out/bin/$executable"
@@ -356,7 +356,7 @@ in
           ExecStart = escapeSystemdExecArgs (
             [
               (lib.getExe cfg.package)
-              "--config=${configFile}"
+              "--config=/etc/knot/knot.conf"
               "--socket=${socketFile}"
             ]
             ++ cfg.extraArgs