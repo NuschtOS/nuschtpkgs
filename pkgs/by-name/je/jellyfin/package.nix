2f75846cd23825533175a84466e6a84d16bb734e
Marcel <me@m4rc3l.de>
2026-09-26 20:52:47 +0200

    Revert "jellyfin: miscellaneous security fixes from 12.0"
    This reverts commit 599cbfe8ccdc7029962c8f4ab613fed8774b4481.

--- pkgs/by-name/je/jellyfin/package.nix
+++ pkgs/by-name/je/jellyfin/package.nix
@@ -1,7 +1,6 @@
 {
   lib,
   fetchFromGitHub,
-  fetchpatch,
   nixosTests,
   dotnetCorePackages,
   buildDotnetModule,
@@ -23,34 +22,6 @@ buildDotnetModule (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-WB/miD5uwoCY9DcTRRtxxOu9G+jojNGp5FZ0HHDqhys=";
   };
-
-  patches = [
-    # Prevent SSRF, local file disclosure and DoS via external references in SVG rendering.
-    # (No public PR.)
-    (fetchpatch {
-      url = "https://github.com/jellyfin/jellyfin/commit/cefa78fc1de2410e5c5c6da5062c98fe98b22d17.patch";
-      hash = "sha256-TxGo+sLLG+C9omxrwvO6byzw+iRqONVLXrQKwkrY22s=";
-    })
-
-    # Fix MaxLoginAttempts not honored.
-    # https://github.com/jellyfin/jellyfin/pull/17274
-    (fetchpatch {
-      url = "https://github.com/jellyfin/jellyfin/commit/8b826d981bcfec22063d6008e38016f4b77790d0.patch";
-      hash = "sha256-Nav05TcpjBJABybU0BVyJ0UyxKi+6nDNBQ6tjY0DPT8=";
-    })
-
-    # GHSA-9x85-gx46-6522
-    # Reject user impersonation when retrieving private playlist items.
-    # (No public PR.)
-    (fetchpatch {
-      url = "https://github.com/jellyfin/jellyfin/commit/911ac3769cdcce50a8f6e0b3c0739d509bd9a23f.patch";
-      hash = "sha256-MwaTwqhuBc7yWW3cL6htlyDQ9O1wt5iFCOM/oVTi6P0=";
-    })
-
-    # Don't let unauthorized users to list other's private playlists.
-    # https://github.com/jellyfin/jellyfin/pull/17025
-    ./fix-playlist-visibility.patch
-  ];
 
   propagatedBuildInputs = [ sqlite ];
 
